f0f3584d62749a0c200dcb82fde9a2cd06cad593
ShreyanshVennamaneni <144498974+anshu7600@users.noreply.github.com>
2024-09-30 20:26:50 -0500

    DriverConstants

--- roadrunner19897jww/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ roadrunner19897jww/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -21,7 +21,7 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 537;
+    public static final double TICKS_PER_REV = 537.6;
     public static final double MAX_RPM = 312;
 
     /*
@@ -44,9 +44,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 1.8898; // in
+    public static double TRACK_WIDTH = 16.57; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 16; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,7 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+
+    // not done
+    public static double kV =  1.0 / rpmToVelocity(MAX_RPM)
     public static double kA = 0;
     public static double kStatic = 0;
 
@@ -65,10 +67,12 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 55;
-    public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+
+    // could be increased 85% power
+    public static double MAX_VEL = 52.48291908330528;
+    public static double MAX_ACCEL = 52.48291908330528;
+    public static double MAX_ANG_VEL = Math.toRadians(176.88528);
+    public static double MAX_ANG_ACCEL = Math.toRadians(176.88528);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
@@ -93,11 +97,3 @@ public class DriveConstants {
     }
 }
 
-
-
-//Wheel radius - ~2 in
-//Gear ratio - 1
-//Track width = 16 in
-// run using encoder = true
-// Max vel + ~50 in/s
-//
