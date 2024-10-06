8326e672dedaaaa96cac6bcd51854de66cddb105
LawrenceTanFTC <lawrencetan458@gmail.com>
2024-10-06 17:39:10 -0500

    start debug

--- roadrunner19897jww/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ roadrunner19897jww/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Trajectory;
-import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
