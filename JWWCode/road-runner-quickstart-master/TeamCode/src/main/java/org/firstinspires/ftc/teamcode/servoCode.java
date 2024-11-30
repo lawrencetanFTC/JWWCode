8a9d6f11a228229d3bf7d2b1073d5e85272759f3
LawrenceTanFTC <lawrencetan458@gmail.com>
2024-11-30 17:58:28 -0600

    servo test code

--- JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoCode.java
+++ JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoCode.java
@@ -10,34 +10,38 @@ public class servoCode extends OpMode {
 
     Servo leftarmServo;
     Servo rightarmServo;
-    Servo extendServo;
+    Servo leftextendServo;
+    Servo rightextendServo;
     double ArmPos = 0;
     double extendPos = 0;
 
     public void init(){
         leftarmServo = hardwareMap.get(Servo.class, "leftArmServo");
         rightarmServo = hardwareMap.get(Servo.class, "rightArmServo");
-        extendServo = hardwareMap.get(Servo.class, "extendServo");
+        leftextendServo = hardwareMap.get(Servo.class, "leftextendServo");
+        rightextendServo = hardwareMap.get(Servo.class, "rightextendServo");
     }
 
     public void loop(){
        if(gamepad1.x){
            ArmPos = ArmPos + .1;
-           leftarmServo.setPosition(ArmPos);
+           leftarmServo.setPosition(-ArmPos);
            rightarmServo.setPosition(ArmPos);
 
        } else if(gamepad1.y){
            ArmPos = ArmPos - .1;
-           leftarmServo.setPosition(ArmPos);
+           leftarmServo.setPosition(-ArmPos);
            rightarmServo.setPosition(ArmPos);
        }
 
        if(gamepad1.a){
            extendPos = extendPos + .1;
-           extendServo.setPosition(extendPos);
+           leftextendServo.setPosition(-extendPos);
+           rightextendServo.setPosition(extendPos);
        } else if(gamepad1.b){
            extendPos = extendPos - .1;
-           extendServo.setPosition(extendPos);
+           leftextendServo.setPosition(-extendPos);
+           rightextendServo.setPosition(extendPos);
        }
 
 
