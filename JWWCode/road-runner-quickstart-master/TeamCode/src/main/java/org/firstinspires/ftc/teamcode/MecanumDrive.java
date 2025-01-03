cd8330a88f25f8f1a192d9a053bf85e8c5ce1c9a
A_Dev484 <77778966+arnavsutar@users.noreply.github.com>
2025-01-03 16:37:49 -0600

    i guess we using motor encoders now
    yea offical decison by current coding lead (shreynashe)

--- JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -236,7 +236,7 @@ public final class MecanumDrive {
 
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
-        localizer = new TwoDeadWheelLocalizer(hardwareMap, lazyImu.get(), PARAMS.inPerTick);
+        localizer = new DriveLocalizer(pose);
 
         FlightRecorder.write("MECANUM_PARAMS", PARAMS);
     }
--- JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
+++ JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
@@ -50,7 +50,7 @@ public final class TwoDeadWheelLocalizer implements Localizer {
         perp = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "frontRight")));
 
         // TODO: reverse encoder directions if needed
-           par.setDirection(DcMotorSimple.Direction.REVERSE);
+        //   par.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.imu = imu;
 
