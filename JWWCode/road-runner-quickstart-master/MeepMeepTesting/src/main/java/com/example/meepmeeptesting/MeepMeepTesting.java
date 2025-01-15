42ff94ffb7251f19fb1d5273f0ef3e2eaa1d7336
LawrenceTanFTC <lawrencetan458@gmail.com>
2025-01-14 18:06:05 -0600

    meepmeep

--- JWWCode/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ JWWCode/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,6 +9,9 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -26,10 +29,28 @@ public class MeepMeepTesting {
                 .splineToSplineHeading(new Pose2d(63.24, -26.73, Math.toRadians(0.00)), Math.toRadians(72.99))
                 .build());
 
+
+
+        myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(-2.4,35.45, Math.toRadians(-90.00)))
+                .splineTo(new Vector2d(-22.57, 48.29), Math.toRadians(211.48))
+                .splineTo(new Vector2d(-48.29, 9.43), Math.toRadians(199.24))
+                .turn(Math.PI/2)
+                .splineTo(new Vector2d(-49.43, 21.43), Math.toRadians(79.66))
+                .splineTo(new Vector2d(-50.57, 58.00), Math.toRadians(92.69))
+                .splineTo(new Vector2d(-47.71, 3.14), Math.toRadians(-87.02))
+                .splineTo(new Vector2d(-57.71, 19.71), Math.toRadians(127.37))
+                .splineTo(new Vector2d(-56.86, 31.43), Math.toRadians(76.33))
+                .splineTo(new Vector2d(-57.14, 41.14), Math.toRadians(79.76))
+                .splineTo(new Vector2d(-58.57, 64.00), Math.toRadians(91.11))
+                .splineTo(new Vector2d(-68.57, -0.29), Math.toRadians(261.16))
+                .splineTo(new Vector2d(-67.43, 64.00), Math.toRadians(88.98))
+
+                .build());
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(myBot2)
                 .start();
     }
 }
