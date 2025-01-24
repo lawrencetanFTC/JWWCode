b0e8cab4b0c0f8ed9e539c843e36a569334b3154
LawrenceTanFTC <lawrencetan458@gmail.com>
2025-01-24 16:49:45 -0600

    paths

--- JWWCode/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ JWWCode/road-runner-quickstart-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -44,28 +44,21 @@ public class MeepMeepTesting {
                 .build());
 
 
-        myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(15,62.5, Math.toRadians(-90.00)))
-                .splineTo(new Vector2d(53.00, 53.00), Math.toRadians(45))
-                .turn(Math.toRadians(-135))
-                .strafeTo(new Vector2d(48, 51))
-                .strafeTo(new Vector2d(48, 48))
-                .splineTo(new Vector2d(53.00, 53.00), Math.toRadians(45))
-                .turn(Math.toRadians(-135))
-                .strafeTo(new Vector2d(58,51))
-                .strafeTo(new Vector2d(58, 48))
-                .splineTo(new Vector2d(53.00, 53.00), Math.toRadians(45))
-                .splineToLinearHeading(new Pose2d(47,27, Math.toRadians(0)), Math.toRadians(0))
-                .strafeTo(new Vector2d(48, 27))
-                .splineTo(new Vector2d(53.00, 53.00), Math.toRadians(45))
-                .splineTo(new Vector2d(52.00, 45.00), Math.toRadians(238.03))
-                .splineToLinearHeading(new Pose2d(25.00, 13.00, Math.toRadians(180.00)), Math.toRadians(180.00))
+        myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(15,61, Math.toRadians(-90.00)))
+                .strafeToConstantHeading(new Vector2d(0, 47))
+                .strafeToConstantHeading(new Vector2d(5, 47))
+                .splineToConstantHeading(new Vector2d(36.50, 24.00), Math.toRadians(-90.00))
+                .splineToConstantHeading(new Vector2d(48.00, 0.00), Math.toRadians(0.00))
+                .strafeToConstantHeading(new Vector2d(48,52.5))
+                .splineToConstantHeading(new Vector2d(58,0), Math.toRadians(0.00))
+                .strafeToConstantHeading(new Vector2d(58,49))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
 
-                .addEntity(myBot)
+                .addEntity(myBot2)
                 .start();
     }
 }
