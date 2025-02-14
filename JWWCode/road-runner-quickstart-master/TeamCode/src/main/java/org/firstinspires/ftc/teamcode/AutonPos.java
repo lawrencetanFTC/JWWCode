7fae7c31684745d660406adce19e5f43addfdb00
AxeMinecraft <lionguard115@gmail.com>
2025-02-15 04:43:59 +0530

    new files

--- JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonPos.java
+++ JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonPos.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
 
 public class AutonPos extends OpMode {
     private DcMotor leftSlideMotor;
@@ -26,7 +30,6 @@ public class AutonPos extends OpMode {
     private final int MAX_POSITION = 8100; // Maximum encoder ticks (full extension)
     private final int MIN_POSITION = 0;
 
-    @Override
     public void init() {
         leftSlideMotor = hardwareMap.get(DcMotor.class, "leftSlideMotor");
         rightSlideMotor = hardwareMap.get(DcMotor.class, "rightSlideMotor");
@@ -54,105 +57,293 @@ public class AutonPos extends OpMode {
         clawWrist = hardwareMap.get(Servo.class, "clawWrist");
     }
 
-  
-    public void openClawTop() {
-        clawServoTop.setPosition(.3511);
+
+//    public Action openClawTop() {
+//        clawServoTop.setPosition(.3511);
+//        return null;
+//    }
+
+    public Action openClawTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.3511);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void closeClawTop() {
-        clawServoTop.setPosition(0.0889);
+
+
+    public Action closeClawTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.0889);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void startPivotTop () {
-        topPivot.setPosition(.7328);
+
+    public Action startPivotTop () {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.7328);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void endPivotTop() {
-        topPivot.setPosition(.0322);
+
+    public Action endPivotTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.0322);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void sampleGrabWristTop() {
-        clawWrist.setPosition(.0744);
+
+    public Action sampleGrabWristTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.0744);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void specimenGrabWristTop() {
-        clawWrist.setPosition(.4883);
+
+    public Action specimenGrabWristTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.4883);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void sampleScoreWristTop() {
-        clawWrist.setPosition(.6856);
+
+    public Action sampleScoreWristTop() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.6856);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void specimenScoreWristTop () {
-        clawWrist.setPosition(.15);
+
+    public Action specimenScoreWristTop () {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                clawServoTop.setPosition(0.15);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void startShoulder() {
-        shoulderLeft.setPosition(.6861);
-        shoulderRight.setPosition(.2661);
+
+    public Action startShoulder() {
+        return new Action() {
+            @Override
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                return false;
+            }
+
+            public boolean run(double deltaTime) {
+                shoulderLeft.setPosition(0.6861);
+                shoulderRight.setPosition(0.2661);
+                return true; // This ensures the action completes instantly
+            }
+
+            public void start() {}
+
+            public void stop() {}
+
+            public boolean isFinished() {
+                return true; // Instantly finishes
+            }
+        };
     }
-    
-    public void sampleGrabShoulder() {
+
+    public Action sampleGrabShoulder() {
         shoulderLeft.setPosition(.8828);
         shoulderRight.setPosition(.0717);
+        return null;
     }
-    
-    public void basketShoulder() {
+
+    public Action basketShoulder() {
         shoulderLeft.setPosition(.4111);
         shoulderRight.setPosition(.5839);
+        return null;
     }
-    
-    public void specimenGrabShoulder() {
+
+    public Action specimenGrabShoulder() {
         shoulderLeft.setPosition(.8983);
         shoulderRight.setPosition(.05);
+        return null;
     }
-    
-    public void specimenScoreShoulder() {
+
+    public Action specimenScoreShoulder() {
         shoulderLeft.setPosition(.1461);
         shoulderRight.setPosition(.8011);
+        return null;
     }
-    
-    public void retract() {
+
+    public Action retract() {
         extendServo.setPosition(0);
+        return null;
     }
-    
-    public void extend() {
+
+    public Action extendForm() {
         extendServo.setPosition(.56);
+        return null;
     }
-    
-    public void downWristBottom() {
+
+    public Action downWristBottom() {
         wristRight.setPosition(.9222);
+        return null;
     }
-    
-    public void middleWristBottom() {
+
+    public Action middleWristBottom() {
         wristRight.setPosition(.5116);
+        return null;
     }
-    
-    public void scanWristBottom() {
+
+    public Action scanWristBottom() {
         wristRight.setPosition(.8206);
+        return null;
     }
-    
-    public void upWristBottom() {
+
+    public Action upWristBottom() {
         wristRight.setPosition(.0978);
+        return null;
     }
-    
-    public void openClawBottom() {
+
+    public Action openClawBottom() {
         clawServoBottom.setPosition(.3383);
+        return null;
     }
-    
-    public void closeClawBottom() {
+
+    public Action closeClawBottom() {
         clawServoBottom.setPosition(.5139);
+        return null;
     }
-    
-    public void tightCloseClawBottom() {
+
+    public Action tightCloseClawBottom() {
         clawServoBottom.setPosition(.5978);
+        return null;
     }
-    
-    public void startPivotBottom () {
+
+    public Action startPivotBottom () {
         bottomPivot.setPosition(.0367);
+        return null;
     }
-    
-    public void endPivotBottom () {
+
+    public Action endPivotBottom () {
         bottomPivot.setPosition(.4033);
+        return null;
+    }
+    public void loop() {
+        // Needed to add this statement
     }
 }
+
+
--- JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamplePathBasket.java
+++ JWWCode/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamplePathBasket.java
@@ -1,21 +1,68 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
 import org.firstinspires.ftc.teamcode.HelloAuto.*;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "BaskettAuto", group = "Autonomous")
 public class SamplePathBasket extends LinearOpMode {
+
+    private AutonPos ObjectPos;
+
+    private DcMotor leftSlideMotor;
+    private DcMotor rightSlideMotor;
+
+    // Servos
+    private Servo topPivot;
+    private Servo bottomPivot;
+    private Servo shoulderLeft;
+    private Servo shoulderRight;
+    private Servo clawServoBottom;
+    private Servo clawServoTop;
+    private Servo clawWrist;
+    private Servo extendServo;
+    private Servo wristLeft;
+    private Servo wristRight;
+
+    // Slide boundaries
+    private final int MAX_POSITION = 8100; // Maximum encoder ticks (full extension)
+    private final int MIN_POSITION = 0;
+
+    public void change() {
+        leftSlideMotor = hardwareMap.get(DcMotor.class, "leftSlideMotor");
+        rightSlideMotor = hardwareMap.get(DcMotor.class, "rightSlideMotor");
+        // Reset encoders for the slide motors
+        leftSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // Set them to run using encoders after reset
+        leftSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        shoulderLeft = hardwareMap.get(Servo.class, "shoulderLeft");
+        shoulderRight = hardwareMap.get(Servo.class, "shoulderRight");
+
+        extendServo = hardwareMap.get(Servo.class, "extendServo");
+
+        wristRight = hardwareMap.get(Servo.class, "wristRight");
+        wristLeft = hardwareMap.get(Servo.class, "wristLeft");
+
+        clawServoBottom = hardwareMap.get(Servo.class, "clawServoBottom");
+        clawServoTop = hardwareMap.get(Servo.class, "clawServoTop");
+
+        topPivot = hardwareMap.get(Servo.class, "topPivot");
+        bottomPivot = hardwareMap.get(Servo.class, "bottomPivot");
+
+        clawWrist = hardwareMap.get(Servo.class, "clawWrist");
+    }
 
     public void runOpMode() {
 
@@ -28,8 +75,6 @@ public class SamplePathBasket extends LinearOpMode {
         Arm arm = new Arm(hardwareMap);
         BClaw claw = new BClaw(hardwareMap);
 
-
-
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
         Action MoveToSubmersible = drive.actionBuilder(initialPose)
@@ -40,17 +85,21 @@ public class SamplePathBasket extends LinearOpMode {
                 .strafeToConstantHeading(new Vector2d(5, 40))
                 .splineToLinearHeading(new Pose2d(36.50, 24.00, Math.toRadians(90.00)), Math.toRadians(-90)) //
                 .splineToConstantHeading(new Vector2d(48.00, 0.00), Math.toRadians(0.00))
+                .build();
 
         Action FirstOrientBucket = drive.actionBuilder(new Pose2d(48, 0, Math.toRadians(90)))
                 .strafeToLinearHeading(new Vector2d(42, 52.5), Math.toRadians(-135))
                 .strafeToConstantHeading(new Vector2d(48, 52.5)) //
+                .build();
             
         Action SecondPositionBucket = drive.actionBuilder(new Pose2d(48, 52.5, Math.toRadians(-135)))
                 .strafeToConstantHeading(new Vector2d(46.5, 52.5))
                 .splineToLinearHeading(new Pose2d(58, 0, Math.toRadians(90.00)), Math.toRadians(0.00))
+                .build();
 
         Action SecondOrientBucket = drive.actionBuilder(new Pose2d(58, 0, Math.toRadians(90)))
                 .strafeToLinearHeading(new Vector2d(48, 52.5), Math.toRadians(-135))
+                .build();
             
         Action PushSamples = drive.actionBuilder(initialPose)
                 .strafeToConstantHeading(new Vector2d(0, 40))
@@ -62,7 +111,7 @@ public class SamplePathBasket extends LinearOpMode {
                 .strafeToConstantHeading(new Vector2d(58, 49))
                 .build();
 
-        Action BucketCase = drive.actionBuilder(new Pose2d(initialPose)
+        Action BucketCase = drive.actionBuilder(initialPose)
                 .strafeToConstantHeading(new Vector2d(0, 40))
                 .strafeToConstantHeading(new Vector2d(5, 40))
                 .splineToLinearHeading(new Pose2d(36.50, 24.00, Math.toRadians(90.00)), Math.toRadians(-90)) //
@@ -73,6 +122,31 @@ public class SamplePathBasket extends LinearOpMode {
                 .splineToLinearHeading(new Pose2d(58, 0, Math.toRadians(90.00)), Math.toRadians(0.00))
                 .strafeToLinearHeading(new Vector2d(48, 52.5), Math.toRadians(-135))
                 .build();
+
+        Action openClawTop = ObjectPos.openClawTop();
+        Action closeClawTop = ObjectPos.closeClawTop();
+        Action startPivotTop = ObjectPos.startPivotTop();
+        Action endPivotTop = ObjectPos.endPivotTop();
+        Action sampleGrabWristTop = ObjectPos.sampleGrabWristTop();
+        Action specimenGrabWristTop = ObjectPos.specimenGrabWristTop();
+        Action sampleScoreWristTop = ObjectPos.sampleScoreWristTop();
+        Action specimenScoreWristTop = ObjectPos.specimenScoreWristTop();
+        Action startShoulder = ObjectPos.startShoulder();
+        Action sampleGrabShoulder = ObjectPos.sampleGrabShoulder();
+        Action basketShoulder = ObjectPos.basketShoulder();
+        Action specimenGrabShoulder = ObjectPos.specimenGrabShoulder();
+        Action specimenScoreShoulder = ObjectPos.specimenScoreShoulder();
+        Action retract = ObjectPos.retract();
+        Action extendForm = ObjectPos.extendForm();
+        Action downWristBottom = ObjectPos.downWristBottom();
+        Action middleWristBottom = ObjectPos.middleWristBottom();
+        Action scanWristBottom = ObjectPos.scanWristBottom();
+        Action upWristBottom = ObjectPos.upWristBottom();
+        Action openClawBottom = ObjectPos.openClawBottom();
+        Action closeClawBottom = ObjectPos.closeClawBottom();
+        Action tightCloseClawBottom = ObjectPos.tightCloseClawBottom();
+        Action startPivotBottom = ObjectPos.startPivotBottom();
+        Action endPivotBottom = ObjectPos.endPivotBottom();
 
         boolean AreWeDoingBucket = true;
 //        boolean AreWeTestingPaths = true;
@@ -86,7 +160,10 @@ public class SamplePathBasket extends LinearOpMode {
             Actions.runBlocking(
                     new SequentialAction(
                             MoveToSubmersible,
-                            new Paralell Action(
+                            openClawTop,
+                            new ParallelAction(
+
+                            ),
 
                             PushSamples
 
